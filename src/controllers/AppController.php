1a13c5c08c621d664ac760473cf1df6496fadccf
UK-laptop <lukasz.les32@student.pk.edu.pl>
2025-08-06 14:07:17 +0200

    WIP

--- src/controllers/AppController.php
+++ src/controllers/AppController.php
@@ -1,12 +1,12 @@
 <?php
 
 class AppController {
-    protected function render(string $template = null) {  //, array $variables = []) {
-        $templatePath = 'public/views/'.$template.'.html';
+    protected function render(string $template = null, array $variables = []) {
+        $templatePath = 'public/views/'.$template.'.php';
         $output = 'File not found';
 
         if(file_exists($templatePath)) {
-            //extract($variables);
+            extract($variables);
 
             ob_start();
             include $templatePath;
